e5912f7a01013e38cfe363a886ce08b1539dfbcc
jankoczuba <jan.koczuba@it-solve.pl>
2024-05-19 11:48:17 +0200

    added basic bottom menu navigation

--- app/src/main/java/com/koczuba/kotlin_food_app/MainActivity.kt
+++ app/src/main/java/com/koczuba/kotlin_food_app/MainActivity.kt
@@ -2,10 +2,26 @@ package com.koczuba.kotlin_food_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.koczuba.kotlin_food_app.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = Navigation.findNavController(this, R.id.frag_host)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
     }
 }
